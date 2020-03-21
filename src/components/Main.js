a42e908b9a81ad66d01fe4d9c3c726410b2cba1f
t.pham@dasgip.de <Pham@zentrale.local>
2020-03-21 17:44:03 +0100

    :construction: Initial implementation of scene A

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,53 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Admission from './scenes/Admission/Admission';
 import './Main.css';
 
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 function App() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (_event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <BrowserRouter>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Social Atmosphere
+          </Typography>
+
+          <Tabs value={value} onChange={handleChange}>
+            <Tab href="/" label="Screen A" {...a11yProps(0)} />
+            <Tab label="Screen B" {...a11yProps(1)} />
+          </Tabs>
+        </Toolbar>
+      </AppBar>
       <Switch>
         <Route path="/">
           <Admission />
--- src/components/scenes/Admission/Admission.js
+++ src/components/scenes/Admission/Admission.js
@@ -1,10 +1,78 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Button from "@material-ui/core/Button";
+import Modal from '@material-ui/core/Modal';
+import Slider from '@material-ui/core/Slider';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+    modalRoot: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+}));
 
 function Admission() {
+    const classes = useStyles();
+
+    const [openModal, setOpenModal] = React.useState(false);
+
+    const handleOpenModal = () => {
+        setOpenModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    };
+
     return (
-        <div>
-            test
-        </div>
+        <Container maxWidth="sm">
+            <Button
+                variant='contained'
+                color='primary'
+                onClick={handleOpenModal}
+            >
+                +
+            </Button>
+            <Modal
+                className={classes.modalRoot}
+                open={openModal}
+                onClose={handleCloseModal}
+            >
+                <form>
+                    <TextField
+                        id="anxiety"
+                        label="What could happen?"
+                        style={{ margin: 8 }}
+                        placeholder="F.e. depression"
+                        fullWidth
+                        margin="normal"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+
+                    <Typography id="input-slider" gutterBottom>
+                        How bad is it?
+                    </Typography>
+                    <Slider />
+
+                    <Typography id="input-slider" gutterBottom>
+                        How likely is it?
+                    </Typography>
+                    <Slider />
+
+                    <Typography id="input-slider" gutterBottom>
+                        How manageable is it?
+                    </Typography>
+                    <Slider />
+
+                </form>
+            </Modal>
+        </Container>
     );
 }
 
