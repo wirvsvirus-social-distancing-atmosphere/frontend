7692d6c2ea05264d1848220914d25036603d486d
Max Dehne <m.dehne@youngdigitals.de>
2020-03-23 13:20:12 +0100

    Histogram: Connect to database

--- src/components/elements/Histogram.js
+++ src/components/elements/Histogram.js
@@ -1,38 +1,99 @@
-import React, { useEffect, useRef } from 'react';
-import { Chart } from 'chart.js';
+import React, {useEffect, useRef} from 'react';
+import {Chart} from 'chart.js';
+import firebase from "../../utils/firebase";
 
-function Histogram({
-    data = {
-        labels: ['15.03.2020', '16.03.2020', '17.03.2020', '18.03.2020', '19.03.2020', '20.03.2020', '21.03.2020'],
-        datasets: [{
-             label: 'World Mood Curve',
-            backgroundColor: 'rgb(54, 162, 235)',
-            borderColor: 'rgb(54, 162, 235)',
-            cubicInterpolationMode: 'monotone',
-            fill: false,
-            data: [100, 125, 191, 185, 215, 225, 128]
-        }]
-    },
-}) {
+function Histogram() {
     const histogramContainer = useRef();
 
-    useEffect(() => {
-        var ctx = histogramContainer.current.getContext('2d');
-        new Chart(ctx, {
-            type: 'line',
-            data,
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                },
-                maintainAspectRatio: false,
-                responsive: true
+    var now = Date.now();
+    var nowRound = Math.round(now);
+    const last4Weeks = nowRound - (2419200000 / 4); //TODO: <- expand to 4 weeks (= remove /4)
+
+    const preparedData = items => {
+        const day = 86400000;
+        const dayArray = [];
+        const valuesPerDayArray = [];
+
+        //TODO: expand to 31 days
+        for (let x = 7; x >= 0; x--) {
+            dayArray.push(new Date(nowRound - (x * day)));
+            valuesPerDayArray.push([])
+        }
+
+        for (let x = 0; x < items.length; x++) {
+            for (let i = 0; i < dayArray.length; i++) {
+                if (dayArray[i].getDate() === new Date(items[x].time).getDate()
+                    && dayArray[i].getMonth() === new Date(items[x].time).getMonth()) {
+                    valuesPerDayArray[i].push(items[x].value)
+                }
             }
-        });
+        }
+
+        let meansPerDay = [];
+        for (let x = 0; x < valuesPerDayArray.length; x++) {
+            let sum = 0;
+            for (let i = 0; i < valuesPerDayArray[x].length; i++) {
+                sum += valuesPerDayArray[x][i]
+            }
+            let mean = sum / valuesPerDayArray[x].length;
+            if (isNaN(mean)) {
+                mean = 50
+            }
+            meansPerDay.push(mean)
+        }
+        const labels = dayArray.map(date => date.getDate() + ".")
+        return {labels: labels, data: meansPerDay}
+    }
+
+    useEffect(() => {
+
+        firebase
+            .firestore()
+            .collection("mood")
+            .where("time", ">=", last4Weeks)
+            .get()
+            .then(function (querySnapshot) {
+                let items = [];
+                querySnapshot.forEach(function (doc) {
+                    items.push(doc.data());
+                });
+                const chartData = preparedData(items);
+                const data = {
+                    labels: chartData.labels,
+                    datasets: [{
+                        label: 'World Mood Curve',
+                        backgroundColor: 'rgb(54, 162, 235)',
+                        borderColor: 'rgb(54, 162, 235)',
+                        cubicInterpolationMode: 'monotone',
+                        fill: false,
+                        data: chartData.data
+                    }]
+                };
+
+                var ctx = histogramContainer.current.getContext('2d');
+                new Chart(ctx, {
+                    type: 'line',
+                    data,
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        },
+                        maintainAspectRatio: false,
+                        responsive: true
+                    }
+                });
+
+
+            })
+            .catch(function (error) {
+                console.log('Error getting documents: ', error);
+            });
+
+
     }, []);
 
     return (
